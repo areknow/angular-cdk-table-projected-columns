6a25d5412ba3ce3e83f00f6a551d7e2f0e9d3774
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-13 14:46:08 -0400

    example component demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  columns = ['id', 'title', 'author'];
+  data = [
+    {
+      id: 1,
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      author: 'J.R.R. Tolkien',
+    },
+    {
+      id: 2,
+      title: 'The Lord of the Rings: The Two Towers',
+      author: 'J.R.R. Tolkien',
+    },
+    {
+      id: 3,
+      title: 'The Lord of the Rings: The Return of the King',
+      author: 'J.R.R. Tolkien',
+    },
+  ];
+}
