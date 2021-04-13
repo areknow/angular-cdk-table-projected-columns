cb2543923d873c42c2e5ee1850fd265e30fea2fe
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-04-13 14:30:16 -0400

    ng generate table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TableComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
